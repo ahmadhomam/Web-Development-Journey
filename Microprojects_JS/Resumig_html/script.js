483518e1600da9581c4c0d046af8811420ae3e73
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2026-09-06 16:25:04 +0530

    selecting from dropdown and changing css

--- Microprojects_JS/Resumig_html/script.js
+++ Microprojects_JS/Resumig_html/script.js
@@ -1,46 +1,20 @@
-let buttons = document.querySelectorAll(".cart_button") ;
-let removes = document.querySelectorAll(".remove")
+let box = document.querySelector("#box")
 
+let select = document.querySelector("#colorSelect")
 
-
-buttons.forEach(button => {
-    button.addEventListener("click",()=>{
-           let quantity = button.parentElement.querySelector(".quantity") ;
-           let total  = button.parentElement.querySelector(".total")
-           let price = button.parentElement.querySelector(".item").getAttribute("data-price")
-           let grandtotal = document.querySelector(".grandtotal")
-
-
-           quantity.textContent ++ ;
-           total.textContent = Number(total.textContent) +  Number(price)
-           grandtotal.textContent= Number(grandtotal.textContent) + Number(price)
-           
-    })
-
+select.addEventListener("change",(dets) => {
+    console.log(dets)
+    let color = dets.target.value.toLowerCase()   ;
+    if(color == "red"){
+        box.style.backgroundColor = "red"   
+    }
+    else if(color == "blue"){
+        box.style.backgroundColor = "blue"
+    }
+    else if(color == "green"){
+        box.style.backgroundColor = "green"
+    }
+    else{
+        box.style.backgroundColor = "white"
+    }
 })
-
-
-removes.forEach(remove => {
-    remove.addEventListener("click",()=>{
-        
-        let quantity = remove.parentElement.querySelector(".quantity")
-        let total = remove.parentElement.querySelector(".total")
-        let price  = remove.parentElement.querySelector(".item").getAttribute("data-price")
-        let grandtotal = document.querySelector(".grandtotal")
-
-
-        if(quantity.textContent <=0){
-            alert("No item in the cart")
-        }
-        else{
-            quantity.textContent-- ;
-            total.textContent = Number(total.textContent) - Number(price)
-            grandtotal.textContent= Number(grandtotal.textContent) - Number(price)
-        }
-
-
-
-    })
-})
-
-
